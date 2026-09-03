9631d784a1f2f650b793d2934c4d1691bf8c8b01
dmotte <37443982+dmotte@users.noreply.github.com>
2026-09-03 13:12:41 +0200

    fix(logtosupd-process): remove default file_out value

--- build/logtosupd/process.py
+++ build/logtosupd/process.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-import sys
-
 from typing import TextIO
 
 from supervisor.events import ProcessLogEvent
 
 
 def exec(event: ProcessLogEvent, headers: dict[str, str],
-         text_lines: list[str], file_out: TextIO = sys.stdout) -> None:
+         text_lines: list[str], file_out: TextIO) -> None:
     prefix = f'logtosupd {headers['processname']} {headers['channel']} | '
     print('\n'.join(f'{prefix}{line}' for line in text_lines), file=file_out)
